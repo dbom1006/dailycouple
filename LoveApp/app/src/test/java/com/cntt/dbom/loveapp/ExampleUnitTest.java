9dd8ad2f3263c5a57d7b39e7056263a2eb41b950
Thái Trung Đức <dbom.1006@gmail.com>
2016-11-01 14:35:34 +0700

    no message

---
+++ LoveApp/app/src/test/java/com/cntt/dbom/loveapp/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.cntt.dbom.loveapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
