21aac681e1bbc10e8593191b7691d386ca043d36
bacnice <xuanbac216@gmail.com>
2016-11-05 22:27:39 +0700

    pick img

--- LoveApp/app/src/main/java/com/cntt/dbom/loveapp/ProfileActivity.java
+++ LoveApp/app/src/main/java/com/cntt/dbom/loveapp/ProfileActivity.java
@@ -2,31 +2,49 @@ package com.cntt.dbom.loveapp;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.cntt.dbom.loveapp.Entity.Relationship;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Calendar;
 import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
+    private int PICK_IMAGE_REQUEST = 1;
+    private String selectedImagePath;
+    Bitmap bitmap;
     Button btnSetDate;
     int year_x, month_x, day_x;
     static final int DILOG_ID = 0, DILOG_ID_MEN = 1, DILOG_ID_WOMEN = 2;
-
     TextView txtBdateMen, txtBdateWomen, txtDStart;
+
+    // image
+    ImageView imgMan, imgWomen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        imgMan = (ImageView) findViewById(R.id.image_avatar_man);
+        imgWomen = (ImageView) findViewById(R.id.image_avatar_women) ;
+
         Spinner spRela=(Spinner) findViewById(R.id.relationship);
         List<Relationship> lst=Relationship.getList();
         ArrayAdapter<Relationship> adapter = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,lst);
@@ -41,6 +59,29 @@ public class ProfileActivity extends AppCompatActivity {
         showDialogOnButtonClick();
     }
     public void showDialogOnButtonClick(){
+        imgMan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), PICK_IMAGE_REQUEST);
+                imgMan.setImageBitmap(bitmap);
+            }
+        });
+        imgWomen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), PICK_IMAGE_REQUEST);
+                //imgMan.setImageBitmap(bitmap);
+            }
+        });
+
         btnSetDate = (Button) findViewById(R.id.dateBtn);
         txtBdateMen = (TextView) findViewById(R.id.Bday_Men);
         txtBdateWomen = (TextView) findViewById(R.id.Bday_Women);
@@ -66,6 +107,60 @@ public class ProfileActivity extends AppCompatActivity {
 
     }
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+            InputStream inputStream;
+            try {
+                inputStream = getContentResolver().openInputStream(uri);
+                bitmap = BitmapFactory.decodeStream(inputStream);
+                // Log.d(TAG, String.valueOf(bitmap));
+                //ImageView imageView = (ImageView) findViewById(R.id.imgTest) ;
+                //imageView.setImageBitmap(bitmap);
+                imgMan.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (resultCode == RESULT_OK) {
+//            if (requestCode == PICK_IMAGE_REQUEST) {
+//                Uri selectedImageUri = data.getData();
+//                selectedImagePath = getPath(selectedImageUri);
+//
+//            }
+//        }
+//    }
+
+    /**
+     * helper to retrieve the path of an image URI
+     */
+    public String getPath(Uri uri) {
+        // just some safety built in
+        if( uri == null ) {
+            // TODO perform some logging or show user feedback
+            return null;
+        }
+        // try to retrieve the image from the media store first
+        // this will only work for images selected from gallery
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        // this is our fallback here
+        return uri.getPath();
+    }
+
+
+    @Override
     protected Dialog onCreateDialog(int id){
         if(id==DILOG_ID_MEN){
             return  new DatePickerDialog(this, dMenPickerListner, year_x, month_x, day_x);
@@ -85,7 +180,7 @@ public class ProfileActivity extends AppCompatActivity {
             year_x = year;
             month_x= month +1;
             day_x = dayOfMonth;
-            txtBdateMen.setText(year_x +"/" + + month_x + "/" + day_x);
+            txtBdateMen.setText(day_x +"/" + month_x + "/" + year_x);
             //Toast.makeText(ProfileActivity.this,year_x +"/" + + month_x + "/" + day_x, Toast.LENGTH_LONG).show();
         }
     };
@@ -94,9 +189,9 @@ public class ProfileActivity extends AppCompatActivity {
         @Override
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
             year_x = year;
-            month_x= month;
+            month_x= month+1;
             day_x = dayOfMonth;
-            txtBdateWomen.setText(year_x +"/" + + month_x + "/" + day_x);
+            txtBdateWomen.setText(day_x +"/" + month_x + "/" + year_x );
             //Toast.makeText(ProfileActivity.this,year_x +"/" + + month_x + "/" + day_x, Toast.LENGTH_LONG).show();
         }
     };
@@ -105,10 +200,12 @@ public class ProfileActivity extends AppCompatActivity {
         @Override
         public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
             year_x = year;
-            month_x= month;
+            month_x= month+1;
             day_x = dayOfMonth;
-            txtDStart.setText(year_x +"/" + + month_x + "/" + day_x);
+            btnSetDate.setText(day_x +"/" +  month_x + "/" + year_x);
             //Toast.makeText(ProfileActivity.this,year_x +"/" + + month_x + "/" + day_x, Toast.LENGTH_LONG).show();
         }
     };
+
+
 }
