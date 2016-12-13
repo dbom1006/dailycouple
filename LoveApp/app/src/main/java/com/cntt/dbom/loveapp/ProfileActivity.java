02c09c33bf9906d3eeef91d85932e0f90c8fddea
bacnice <xuanbac216@gmail.com>
2016-12-13 11:35:24 +0700

    mo` home

--- LoveApp/app/src/main/java/com/cntt/dbom/loveapp/ProfileActivity.java
+++ LoveApp/app/src/main/java/com/cntt/dbom/loveapp/ProfileActivity.java
@@ -14,6 +14,7 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -122,7 +123,13 @@ public class ProfileActivity extends AppCompatActivity {
             else imgWomen.setImageURI(uri);
         }
     }
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.lists_menu, menu);
+        return true;
+    }
+    //    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 //        if (resultCode == RESULT_OK) {
 //            if (requestCode == PICK_IMAGE_REQUEST) {
 //                Uri selectedImageUri = data.getData();
--- LoveApp/app/src/main/java/com/cntt/dbom/loveapp/TimeLineActivity.java
+++ LoveApp/app/src/main/java/com/cntt/dbom/loveapp/TimeLineActivity.java
@@ -2,6 +2,7 @@ package com.cntt.dbom.loveapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class TimeLineActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class TimeLineActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_line);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.lists_menu, menu);
+        return true;
     }
 }
