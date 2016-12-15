d8cd987970b2e990df066c7abc9e5fab8139d52c
Thái Trung Đức <dbom.1006@gmail.com>
2016-12-15 15:45:19 +0700

    Done Event

--- LoveApp/app/src/main/java/com/cntt/dbom/loveapp/DAL/DBContext.java
+++ LoveApp/app/src/main/java/com/cntt/dbom/loveapp/DAL/DBContext.java
@@ -25,6 +25,78 @@ public class DBContext extends SQLiteOpenHelper {
                 "    DateBegin    DATE,\n" +
                 "    Relationship VARCHAR\n" +
                 ");\n");
+        db.execSQL("CREATE TABLE Event (\n" +
+                "    Name VARCHAR (50) NOT NULL,\n" +
+                "    Date VARCHAR,\n" +
+                "    Type INTEGER,\n" +
+                "    Icon INTEGER\n" +
+                ");\n");
+        db.execSQL("INSERT INTO Event (\n" +
+                "                      Icon,\n" +
+                "                      Type,\n" +
+                "                      Date,\n" +
+                "                      Name\n" +
+                "                  )\n" +
+                "                  VALUES (\n" +
+                "                      2130837588,\n" +
+                "                      1,\n" +
+                "                      '24/12/2016',\n" +
+                "                      'Noel'\n" +
+                "                  ),\n" +
+                "                  (\n" +
+                "                      2130837588,\n" +
+                "                      1,\n" +
+                "                      '01/01/2017',\n" +
+                "                      'Tết dương lịch'\n" +
+                "                  ),\n" +
+                "                  (\n" +
+                "                      2130837587,\n" +
+                "                      1,\n" +
+                "                      '14/02/2017',\n" +
+                "                      'Valentine'\n" +
+                "                  ),\n" +
+                "                  (\n" +
+                "                      2130837586,\n" +
+                "                      1,\n" +
+                "                      '22/02/1995',\n" +
+                "                      'Sinh nhật Anh'\n" +
+                "                  ),\n" +
+                "                  (\n" +
+                "                      2130837586,\n" +
+                "                      1,\n" +
+                "                      '28/06/1995',\n" +
+                "                      'Sinh nhật Em'\n" +
+                "                  ),\n" +
+                "                  (\n" +
+                "                      2130837588,\n" +
+                "                      1,\n" +
+                "                      '08/03/2017',\n" +
+                "                      'Phụ nữ Việt Nam'\n" +
+                "                  ),\n" +
+                "                  (\n" +
+                "                      2130837588,\n" +
+                "                      1,\n" +
+                "                      '01/06/2017',\n" +
+                "                      'Quốc tế thiếu nhi'\n" +
+                "                  ),\n" +
+                "                  (\n" +
+                "                      2130837587,\n" +
+                "                      2,\n" +
+                "                      '25/06/2017',\n" +
+                "                      'Dẫn gấu đi họp lớp'\n" +
+                "                  ),\n" +
+                "                  (\n" +
+                "                      2130837587,\n" +
+                "                      2,\n" +
+                "                      '15/01/2017',\n" +
+                "                      'Dẫn gấu di mua đồ tết'\n" +
+                "                  ),\n" +
+                "                  (\n" +
+                "                      2130837587,\n" +
+                "                      2,\n" +
+                "                      '12/10/2016',\n" +
+                "                      'Dẫn gấu đi xem phim'\n" +
+                "                  );\n");
         db.execSQL("INSERT INTO Profile (\n" +
                 "                        Relationship,\n" +
                 "                        DateBegin,\n" +
--- LoveApp/app/src/main/java/com/cntt/dbom/loveapp/EventActivity.java
+++ LoveApp/app/src/main/java/com/cntt/dbom/loveapp/EventActivity.java
@@ -2,81 +2,125 @@ package com.cntt.dbom.loveapp;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.Intent;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
+import android.widget.AdapterView;
+import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.cntt.dbom.loveapp.DAL.EventDAO;
 import com.cntt.dbom.loveapp.Entity.Event;
+import com.cntt.dbom.loveapp.adapter.EventAdapter;
 
 import java.util.List;
 
-public class EventActivity extends AppCompatActivity {
-
+public class EventActivity extends AppCompatActivity{
+    public EventAdapter adapter;
+    public List<Event> lst;
+    public ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event);
-        List<Event> lst=Event.getList();
-        EventAdapter adapter = new EventAdapter(this, R.layout.list_events,lst);
-        ListView listView = (ListView) findViewById(R.id.listEvent);
+        lst=EventDAO.getList(EventActivity.this);
+        adapter = new EventAdapter(this, R.layout.list_events,lst);
+        listView = (ListView) findViewById(R.id.listEvent);
         listView.setAdapter(adapter);
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog dialog=AskOption(lst.get(position),EventActivity.this);
+                dialog.show();
+                return false;
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.add_menu, menu);
         return true;
     }
+
     public void openDialog(Context context) {
-        final Dialog dialog = new Dialog(context); // Context, this, etc.
-        dialog.setContentView(R.layout.dialog_create_event);
-        dialog.setTitle("Add event");
-        dialog.show();
+        AlertDialog.Builder builder;
+        AlertDialog alertDialog;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View layout = inflater.inflate(R.layout.dialog_create_event, null);
+        builder = new AlertDialog.Builder(context);
+        builder.setView(layout);
+        builder.setNegativeButton("Đồng ý", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                createEvent(layout,EventActivity.this);
+                lst=EventDAO.getList(EventActivity.this);
+                adapter.clear();
+                adapter.addAll(lst);
+            }
+        });
+        builder.setPositiveButton("Hủy", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alertDialog = builder.create();
+        alertDialog.show();
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.addEvent) {
-
             openDialog(this);
             return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    class EventAdapter extends ArrayAdapter<Event> {
-        public List<Event> data;
+    public void createEvent(View view,Context context){
+        TextView txtName=(TextView) view.findViewById(R.id.txtCreateEventName);
+        TextView txtDate=(TextView) view.findViewById(R.id.txtCreateEventDate);
+        CheckBox chkIcon=(CheckBox) view.findViewById(R.id.chkCreateEventImportant);
+        CheckBox chkType=(CheckBox) view.findViewById(R.id.chkCreateEventEveryYear);
+        String name=txtName.getText().toString();
+        String date=txtDate.getText().toString();
+        int icon=chkIcon.isChecked()?R.drawable.calendar_important:R.drawable.calendar_love;
+        int type=chkType.isChecked()?1:2;
+        Event e=new Event(name,date,type,icon);
+        EventDAO.Insert(e,context);
+    }
+    private AlertDialog AskOption(final Event e,final Context context)
+    {
 
-        public EventAdapter(Context context, int resource, List<Event> objects) {
-            super(context, resource, objects);
-            this.data=objects;
-        }
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View view=getLayoutInflater().inflate(R.layout.list_events,null);
-            if(position==0 || !data.get(position).getMonthYear().equals(data.get(position-1).getMonthYear())){
-                TextView txtListEventHeader=(TextView) view.findViewById(R.id.txtListEventHeader);
-                String[] s=data.get(position).getMonthYear().split("/");
-                String t="Tháng "+s[0]+" Năm "+s[1];
-                txtListEventHeader.setText(t);
-                txtListEventHeader.setVisibility(View.VISIBLE);
-            }
-            TextView txtName=(TextView) view.findViewById(R.id.txtListEventName);
-            TextView txtDate=(TextView) view.findViewById(R.id.txtListEventDate);
-            TextView txtDaysLeft=(TextView) view.findViewById(R.id.txtListEventDaysLeft);
-            ImageView img=(ImageView) view.findViewById(R.id.iconListEvent);
-            txtDaysLeft.setText("Còn "+data.get(position).getDaysLeft()+" ngày");
-            txtName.setText(data.get(position).getName());
-            txtDate.setText(data.get(position).getDate());
-            img.setImageResource(data.get(position).getIcon());
-            return view;
-        }
+        AlertDialog myQuittingDialogBox =new AlertDialog.Builder(this)
+                //set message, title, and icon
+                .setTitle("Xóa sự kiện")
+                .setMessage("Bạn có muốn xóa sự kiện này?\n"+e.getName()+" - "+e.getFullDate())
+                .setIcon(R.drawable.calendar_delete)
+                .setPositiveButton("Hủy", new DialogInterface.OnClickListener() {
+
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        dialog.dismiss();
+                    }
+
+                })
+                .setNegativeButton("Xóa", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        EventDAO.Delete(e,context);
+                        lst=EventDAO.getList(EventActivity.this);
+                        adapter.clear();
+                        adapter.addAll(lst);
+                        dialog.dismiss();
+                    }
+                })
+                .create();
+        return myQuittingDialogBox;
+
     }
 }
